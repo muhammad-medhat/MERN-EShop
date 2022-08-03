83623ecb2d075ed520f18aa7d1ba69dbd52f2899
Muhammad <Muhammad.medhat@gmail.com>
2022-08-03 18:38:45 +0200

    using hooks in frontend

--- frontend/src/components/screens/homeScreen.js
+++ frontend/src/components/screens/homeScreen.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../../products';
+// import products from '../../products';
 import Product from '../product/product';
+
 const HomeScreen = () => {
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+            .catch(err => console.log(err));
+    }
+    , [])
+
     return ( 
         <>   
         <Row>
@@ -10,7 +21,7 @@ const HomeScreen = () => {
              
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     {/* <Product key={product._id} {...product} /> */}
-                    <Product product={product} />
+                    <Product  product={product} />
                </Col>
             ))}
         </Row>
