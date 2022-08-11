51ed810436fffc3c7625db08da05e5e6b0381b2f
Muhammad <Muhammad.medhat@gmail.com>
2022-08-11 23:17:20 +0200

    chor: loading products using selectors in redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,14 +3,19 @@ import {
   PRODUCTS_LIST_SUCCESS,
   PRODUCTS_LIST_FAIL,
 } from "../const/constants";
-import axios from "axios";
+// import axios from "axios";
 
  export const ListProducts = () => async (dispatch) => {
         try {
             dispatch({ type: PRODUCTS_LIST_REQUEST });
 
-            // const response = await fetch("/api/products");
-            const { data } = await axios.get("/api/products");
+
+            //const { data } = await axios.get("/api/products");
+
+            const response = await fetch("/api/products");            
+            const data = await response.json();
+            
+            console.log('products action',data);
 
             dispatch({ type: PRODUCTS_LIST_SUCCESS, payload: data });
 
--- frontend/src/components/screens/homeScreen.js
+++ frontend/src/components/screens/homeScreen.js
@@ -1,31 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 // import products from '../../products';
 import Product from '../product/product';
+import { ListProducts } from '../../actions/productActions';
+
+
 
 const HomeScreen = () => {
+    const dispatch=useDispatch();
+    const productList = useSelector(state => state.productList)
+    console.log(productList);
+    const{loading, products, error} = productList;
 
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('/api/products')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-            .catch(err => console.log(err));
+        // fetch('/api/products')
+        //     .then(res => res.json())
+        //     .then(data => setProducts(data))
+        //     .catch(err => console.log(err));
+        dispatch(ListProducts());
+
     }
-    , [])
+    , [dispatch])
 
     return ( 
         <>   
-        <Row>
-            {products.map(product => (
-             
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    {/* <Product key={product._id} {...product} /> */}
-                    <Product  product={product} />
-               </Col>
-            ))}
-        </Row>
-
+        {
+            loading 
+            ? (<div>Loading...</div> )
+            : error
+            ? (<h3>{error}</h3>)
+            : (<>
+                <h2>Latest Products</h2>
+                <Row>
+                    {products.map(product => (
+                    
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            {/* <Product key={product._id} {...product} /> */}
+                            <Product  product={product} />
+                    </Col>
+                    ))}
+                </Row>    
+                </>            
+            )
+        }
         </>
      );
 }
