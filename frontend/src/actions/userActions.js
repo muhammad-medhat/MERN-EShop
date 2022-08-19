715a0a50e1ca874514fa8bf9b2ed661c43b74356
Muhammad <Muhammad.medhat@gmail.com>
2022-08-19 18:51:04 +0200

    feat: register action and reducer

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,12 @@ import {
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_REQUEST,
   } from "../const/constants.js";
 export const login = (email, password) => async (dispatch) => {
-    debugger
+    
     try{
         dispatch({
             type: USER_LOGIN_REQUEST
@@ -26,7 +29,7 @@ export const login = (email, password) => async (dispatch) => {
         dispatch({type: USER_LOGIN_SUCCESS, payload: data});
         localStorage.setItem('userInfo', JSON.stringify(data));
 
-    } catch(error){debugger
+    } catch(error){
         dispatch({
             type: USER_LOGIN_FAIL,
             payload: error.response && error.response.data.message 
@@ -50,4 +53,37 @@ export const logout = () => async (dispatch) => {
                 : error.message 
         });
     }
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+    
+    try{
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        });
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            method:'POST', 
+            body:JSON.stringify({name, email, password})
+        };
+
+        
+        const response = await fetch('/api/users', config);
+        const data = await response.json();
+
+        
+        dispatch({type: USER_REGISTER_SUCCESS, payload: data});
+        localStorage.setItem('userInfo', JSON.stringify(data));
+
+    } catch(error){
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message 
+        });
+    }
+
 }
