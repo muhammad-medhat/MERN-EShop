6ee32807d497a5806436e8a763a381bde46dd990
Muhammad <Muhammad.medhat@gmail.com>
2022-09-04 22:35:58 +0200

    feat: creating endpoint /api/orders/:id/pay

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,13 +34,40 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
-export const getOrderById = asyncHandler(async(req, res) => {
+
+export const getOrderById = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  const order = await Order.findById(id).populate('user', 'email name')
+  const order = await Order.findById(id).populate("user", "email name");
   // console.log(`Order: ${Order}`.green)
-  if(!order) {
-      return res.status(404).json({msg: 'Order not found'})
+  if (!order) {
+    return res.status(404).json({ msg: "Order not found" });
   } else {
-      return res.json(order);
+    return res.json(order);
   }
-})
+});
+
+/**
+ * @route PUT /api/Orders/:id/pay
+ * @desc Update Order Payment Status
+ * @access Private
+ */
+export const payOrder = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const order = await Order.findById(id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = order.save();
+
+    return res.json(updatedOrder);
+  } else {
+    res.status(404)
+    throw new Error( "Order not found" );
+  }
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,6 +2,7 @@ import Express from "express";
 import {
   addOrderItems,
   getOrderById,
+  payOrder,
 } from "../controllers/orderController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
@@ -9,5 +10,6 @@ const router = Express.Router();
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, payOrder);
 
 export default router;
