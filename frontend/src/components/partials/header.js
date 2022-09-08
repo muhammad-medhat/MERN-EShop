032a6450b0bedd51f21c7ca1efd66d46a645345a
Muhammad <Muhammad.medhat@gmail.com>
2022-09-08 11:53:05 +0200

    chor: Admin Menu Access Security

--- frontend/src/components/partials/header.js
+++ frontend/src/components/partials/header.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { logout } from '../../actions/userActions'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -11,7 +12,7 @@ const Header = () => {
   const { userInfo } = userLogin
 
   const logoutHandler = () => {
-    dispatch({ type: "USER_LOGOUT" });
+    dispatch(logout())
   }
 
   return (
--- frontend/src/components/screens/usersListScreen.js
+++ frontend/src/components/screens/usersListScreen.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Table, Form, Row, Col, FormControl, FormGroup, Button } from "react-bootstrap";
+import {
+  Table,
+  Form,
+  Row,
+  Col,
+  FormControl,
+  FormGroup,
+  Button,
+} from "react-bootstrap";
 import FormContainer from "../formContainer";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,59 +19,79 @@ import { LinkContainer } from "react-router-bootstrap";
 
 const UsersList = () => {
   const dispatch = useDispatch();
+  const nav = useNavigate();
   const usersList = useSelector((state) => state.userList);
-  const { loading, users, error } = usersList;  
-  
-  const userDelete = useSelector((state) => state.userDelete);
-  const { loading: loadingD, success: successD, errorD } = userDelete;  
+  const { loading, users, error } = usersList;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { loading: loadingD, success: successD, errorD } = userDelete;
+  // debugger
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
   console.log(usersList);
 
-  function deleteHandler(id){
-    dispatch(deleteUser(id))
+  function deleteHandler(id) {
+    dispatch(deleteUser(id));
   }
   useEffect(() => {
-    dispatch(userList());
-  }, [dispatch, successD]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(userList());
+    } else nav("/login");
+  }, [dispatch, successD, userInfo]);
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th> Name</th>
-          <th>email</th>
-          <th>Admin</th>
-          <th>...</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users && users.map((user, index) => (
-          <tr key={index}>
-            <td>{index}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>
-              {user.isAdmin 
-              ? (<i className="fas fa-check"></i>)
-              : (<i className="fas fa-times"></i>)
-              }
-            </td>
-            <td>
-            <LinkContainer to={`users/${user._id}/edit`}>
-              <Button variant="primary">
-                  <i className="fas fa-edit"></i>
-              </Button>
-            </LinkContainer>
-            <Button variant="danger" onClick={()=>deleteHandler(user._id)}>
-                  <i className="fas fa-trash"></i>
-              </Button>
-
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <h2>user list</h2>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th> Name</th>
+                <th>email</th>
+                <th>Admin</th>
+                <th>...</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users &&
+                users.map((user, index) => (
+                  <tr key={index}>
+                    <td>{index}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                      {user.isAdmin ? (
+                        <i className="fas fa-check"></i>
+                      ) : (
+                        <i className="fas fa-times"></i>
+                      )}
+                    </td>
+                    <td>
+                      <LinkContainer to={`users/${user._id}/edit`}>
+                        <Button variant="primary">
+                          <i className="fas fa-edit"></i>
+                        </Button>
+                      </LinkContainer>
+                      <Button
+                        variant="danger"
+                        onClick={() => deleteHandler(user._id)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
   );
 };
 
