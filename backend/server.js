010305a8d49df63cae25d00f8778f87d5422bb04
Muhammad <Muhammad.medhat@gmail.com>
2022-09-23 17:09:43 +0200

    Chor: morgan package for logging requests

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
-import path from 'path'
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import colors from "colors";
-import connectDB from "./config/db.js";
-
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -20,11 +17,11 @@ const app = express();
 
 dotenv.config();
 connectDB();
-const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-};
 
+//morgan configuration
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 //   app.use(cors(corsOptions));
 app.use(express.json()); // for parsing application/json
 
@@ -44,8 +41,8 @@ app.get("/api/config/paypal", (req, res) =>
 //the __dirname is available if we are using commonjs
 // since ES-modules is used in the system
 // we can mimic it using the following line
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Handle errors middleware
 app.use(errorHandler, notFound);
@@ -53,7 +50,6 @@ app.use(errorHandler, notFound);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(
-    `Server is running on the ${process.env.NODE_ENV} mode on port ${port}`
-      .blue
+    `Server is running on the ${process.env.NODE_ENV} mode on port ${port}`.blue
   );
 });
